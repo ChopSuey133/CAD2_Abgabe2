c41692a893e510d9a3d3c4c261c676adf278519f
ChopSuey133 <Hubert.schwaiger@gmx.net>
2024-11-08 16:31:03 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Willkommen zu meiner Frontend-App!</h1>
+      <p>Dies ist eine einfache React-Anwendung in Docker.</p>
+    </div>
+  );
+}
+
+export default App;
